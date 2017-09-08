90d9c34f5e3f126e927b7e725c74a0a58818f8d0
wesackah <wesley.avkah@student.hu.nl>
2017-09-08 14:28:34 +0200

    assignment

---
+++ pe2_2.py
@@ -0,0 +1,7 @@
+cijferICOR = 7
+cijferPROG = 6.5
+cijferCSN = 6
+gemiddelde = (cijferCSN +cijferPROG + cijferICOR)/3
+beloning = 3*gemiddelde*30
+overzicht = f"Mijn cijfers (gemiddeld een {gemiddelde}) leveren een beloning van € {beloning} op!"
+print(overzicht)
