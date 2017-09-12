58c8b7778ba34db399ac380e02c501532ad37860
wesackah <wesley.avkah@student.hu.nl>
2017-09-12 16:40:56 +0200

    assignment

---
+++ les 3/pe3_5.py
@@ -0,0 +1,6 @@
+lst = [0,1,2,3,4,5,6,7,8,9]
+for i in lst:
+    if (i/2).is_integer():
+        print(i)
+
+#hoe kan dit makkelijker? want dit lijkt me niet de methode
---
+++ les 3/pe3_6.py
@@ -0,0 +1,4 @@
+s = "Guido van Rossum heeft programmeertaal Python bedacht."
+for i in s:
+    if i in 'aieou':
+        print(i)
