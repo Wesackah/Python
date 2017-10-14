4a7215771279d689f96e4afa255570e4b16d7d6c
wesackah <wesley.ackah@student.hu.nl>
2017-10-14 13:39:46 +0200

    assignment

---
+++ les 10/pe10_1.py
@@ -0,0 +1,8 @@
+import xmltodict
+
+xmlFile = open('artikelen.xml', 'r')
+xmlDict = xmltodict.parse(xmlFile.read())
+xmlFile.close()
+
+for artikel in xmlDict['artikelen']['artikel']:
+    print('nummer: {}, code: {}, naam: {}, voorraad: {}, prijs: {}'.format(artikel['@nummer'], artikel['code'], artikel['naam'], artikel['voorraad'], artikel['prijs']))
